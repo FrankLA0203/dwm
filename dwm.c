2db0bab99cdf716a06134f74e6b9ac6e88e3887c
FrankLA0203 <frankla0203@outlook.com>
2020-02-22 18:14:00 +0800

    Xresources color

--- dwm.c
+++ dwm.c
@@ -1050,10 +1050,10 @@ loadxrdb()
       xrdb = XrmGetStringDatabase(resm);
 
       if (xrdb != NULL) {
-        XRDB_LOAD_COLOR("*.color0", normbordercolor);
+        XRDB_LOAD_COLOR("*.color1", normbordercolor);
         XRDB_LOAD_COLOR("*.color0", normbgcolor);
         XRDB_LOAD_COLOR("*.color8", normfgcolor);
-        XRDB_LOAD_COLOR("*.color1", selbordercolor);
+        XRDB_LOAD_COLOR("*.color2", selbordercolor);
         XRDB_LOAD_COLOR("*.color2", selbgcolor);
         XRDB_LOAD_COLOR("*.background", selfgcolor);
       }
