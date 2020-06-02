6b914afdd2c2b01f0e8a342b5db3ebcb1d62c207
FrankLA0203 <frankla0203@outlook.com>
2020-06-02 18:44:40 +0800

    Fix conflict

--- config.def.h
+++ config.def.h
@@ -87,6 +87,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     { MODKEY,                       XK_at,     togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_F1,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
