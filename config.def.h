e7c617eea6577a4bf0dbff5727c524f08ea7f03d
FrankLA0203 <frankla0203@outlook.com>
2020-06-02 21:04:35 +0800

    Gap change

--- config.def.h
+++ config.def.h
@@ -3,11 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap        = 0;       /* snap pixel */
-static const unsigned int gappih      = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv      = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh      = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov      = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps            = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih      = 15;       /* horiz inner gap between windows */
+static const unsigned int gappiv      = 15;       /* vert inner gap between windows */
+static const unsigned int gappoh      = 15;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 15;       /* vert outer gap between windows and screen edge */
+static const int smartgaps            = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FuraMono Nerd Font:size=8" };
