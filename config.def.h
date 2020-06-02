677d6992c338ae629951c3bcd14cb909356df653
FrankLA0203 <frankla0203@outlook.com>
2020-06-02 19:12:49 +0800

    Fix conflict

--- config.def.h
+++ config.def.h
@@ -86,8 +86,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_s,      togglescratch,  {SHCMD("st -t 'scratchpad' -g 120x34") } },
 	{ MODKEY,                       XK_F1,     xrdb,           {.v = NULL } },
+	{ MODKEY,                       XK_s,      togglescratch,  {SHCMD("st -t 'scratchpad' -g 120x34") } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
