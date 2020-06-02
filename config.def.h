3ae80fb914a9492fcf2c2546e8752486e2cabd1e
FrankLA0203 <frankla0203@outlook.com>
2020-06-02 18:08:32 +0800

    Binding change

--- config.def.h
+++ config.def.h
@@ -79,7 +79,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
 	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, */
 	{ MODKEY|ShiftMask,             XK_Return, focusmaster,    {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
